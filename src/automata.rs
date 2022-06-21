e70cded4eb0926870fc4e845bd8f4fa22de3651a
Nishant2022 <nishant.dash@gmail.com>
2022-06-20 21:43:31 -0400

    Added cells and rule implementation

---
+++ src/automata.rs
@@ -0,0 +1,143 @@
+use bevy::prelude::*;
+use crate::WinSize;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CellState {
+    Alive,
+    Dead,
+}
+#[derive(Clone, Copy, Component)]
+pub struct Cell {
+    state: CellState,
+    position_x: u32,
+    position_y: u32,
+}
+
+#[derive(Component)]
+struct CellLabel;
+
+struct CellSettings {
+    cell_size: f32,
+    num_cells: u32,
+    dead_color: Color,
+    alive_color: Color,
+    rule: [bool; 8],
+}
+
+struct CellGrid {
+    grid: Vec<Vec<Cell>>,
+}
+
+pub struct AutomataPlugin;
+
+impl Plugin for AutomataPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_startup_system_to_stage(StartupStage::PostStartup ,cell_spawn_system)
+            .add_system(update_cell_grid_system)
+            .add_system(color_grid_system);
+    }
+}
+
+fn cell_spawn_system(mut commands: Commands, win_size: Res<WinSize>) {
+    let num_cells: usize = 129;
+    let cell_size: f32 = win_size.w / num_cells as f32;
+    
+    commands.insert_resource(CellSettings {
+        cell_size: cell_size,
+        num_cells: num_cells as u32,
+        dead_color: Color::BLACK,
+        alive_color: Color::WHITE,
+        rule: get_rule(75),
+    });
+
+    let mut cell_grid = CellGrid {grid: Vec::new()};
+
+    for i in 0..num_cells {
+        cell_grid.grid.push(Vec::new());
+        for j in 0..num_cells {
+            let x_pos =  -win_size.w / 2.0 + cell_size * i as f32;
+            let y_pos =  win_size.h / 2.0 - cell_size * j as f32;
+
+            let new_cell = Cell {
+                state: CellState::Dead,
+                position_x: i as u32,
+                position_y: j as u32,
+            };
+
+            cell_grid.grid[i].push(new_cell.clone());
+
+            commands.spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(cell_size, cell_size)),
+                    ..Default::default()
+                },
+                transform: Transform {
+                    translation: Vec3::new(x_pos, y_pos, 10.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+                .insert(new_cell);
+
+            
+        }
+    }
+    commands.insert_resource(cell_grid);
+}
+
+fn update_cell_grid_system(mut cell_grid: ResMut<CellGrid>, cell_settings: Res<CellSettings>) {
+    let num_cells = cell_settings.num_cells;
+    
+    for i in 0..num_cells {
+        cell_grid.grid[i as usize][0].state = CellState::Dead;
+    }
+
+    cell_grid.grid[(num_cells / 2) as usize][0].state = CellState::Alive;
+
+    for i in 0..num_cells as usize {
+        for j in 1..num_cells as usize {
+
+            let mut counter = 0;
+
+            if i != 0 && cell_grid.grid[i - 1][j - 1].state == CellState::Alive {
+                counter += 4;    
+            }
+            
+            if i != (num_cells - 1) as usize && cell_grid.grid[i + 1][j - 1].state == CellState::Alive {
+                counter += 2;
+            }
+            
+            if cell_grid.grid[i][j - 1].state == CellState::Alive {
+                counter += 1;   
+            }
+
+            if cell_settings.rule[counter as usize] { cell_grid.grid[i][j].state = CellState::Alive}
+            else { cell_grid.grid[i][j].state = CellState::Dead }
+
+        }
+    }
+}
+
+fn color_grid_system(mut query: Query<(&mut Sprite, &mut Cell)>, cell_grid: Res<CellGrid>, cell_settings: Res<CellSettings>) {
+    for (mut sprite, mut cell) in query.iter_mut() {
+        cell.state = cell_grid.grid[cell.position_x as usize][cell.position_y as usize].state;
+        match cell.state {
+            CellState::Alive => sprite.color = cell_settings.alive_color,
+            CellState::Dead => sprite.color = cell_settings.dead_color,
+        }
+    }
+}
+
+fn get_rule(mut rule_num: u8) -> [bool; 8] {
+    let mut rule: [bool; 8] = [false; 8];
+
+    for i in 1..=8 {
+        if rule_num / u8::pow(2, 8 - i) > 0 {
+            rule_num -= u8::pow(2, 8 - i); 
+            rule[8 - i as usize] = true;
+        }
+    }
+
+    return rule;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use automata::AutomataPlugin;
+use bevy::prelude::*;
+
+mod automata;
+
+// section:      resouces
+
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+
+// endsection:   resouces
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: String::from("Nishant's Elementary Cellular Automaton"),
+            width: 640.,
+            height: 360.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AutomataPlugin)
+        .add_startup_system(setup_system)
+        .run();
+}
+
+fn setup_system(
+    mut commands: Commands,
+    mut windows: ResMut<Windows>
+) {
+
+    // Add camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // Add WinSize resource
+    let window = windows.get_primary_mut().unwrap();
+    let (win_w, win_h) = (window.width(), window.height());
+    let win_size = WinSize{w: win_w, h: win_h};
+    commands.insert_resource(win_size);
+
 }
