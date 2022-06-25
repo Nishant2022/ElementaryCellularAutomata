1e6198c0273e38c5dc7d17d8f472291629fdad74
Nishant2022 <nishant.dash@gmail.com>
2022-06-25 19:25:48 -0400

    Added comments

--- src/automata.rs
+++ src/automata.rs
@@ -43,25 +43,34 @@ impl Plugin for AutomataPlugin {
 }
 
 fn cell_spawn_system(mut commands: Commands, win_size: Res<WinSize>) {
-    let num_cells: usize = 129;
-    let cell_size: f32 = win_size.w / num_cells as f32 * 2.0;
+
+    // num_cells defines the length and width of cell grid
+    // should optimally be a value 2^n + 1
+    const NUM_CELLS: usize = 257;
+
+    // cell_size is the size of each cell
+    let cell_size: f32 = win_size.w / NUM_CELLS as f32 * 2.0;
     
+    // init_rule is the initial rule that will be calculated
     let init_rule: u8 = 30;
 
+    // Insert CellSettings resources
     commands.insert_resource(CellSettings {
         cell_size: cell_size,
-        num_cells: num_cells as u32,
+        num_cells: NUM_CELLS as u32,
         dead_color: Color::BLACK,
         alive_color: Color::WHITE,
         rule_num: init_rule,
         rule: get_rule(init_rule),
     });
 
+    // Create cell grid
     let mut cell_grid = CellGrid {grid: Vec::new()};
 
-    for i in 0..num_cells {
+    // Initialize Cell Grid with grid of dead cells
+    for i in 0..NUM_CELLS {
         cell_grid.grid.push(Vec::new());
-        for j in 0..num_cells {
+        for j in 0..NUM_CELLS {
             let x_pos =  -win_size.w / 1.0 + cell_size * i as f32;
             let y_pos =  win_size.h / 1.0 - cell_size * j as f32;
 
@@ -92,18 +101,24 @@ fn cell_spawn_system(mut commands: Commands, win_size: Res<WinSize>) {
     commands.insert_resource(cell_grid);
 }
 
+// System used to update CellGrid resouce 
 fn update_cell_grid_system(mut cell_grid: ResMut<CellGrid>, cell_settings: Res<CellSettings>) {
     let num_cells = cell_settings.num_cells;
-    
+   
+    // Go through first row of cells and set them to Dead
     for i in 0..num_cells {
         cell_grid.grid[i as usize][0].state = CellState::Dead;
     }
 
+    // Set middle cell of first row to Alive
     cell_grid.grid[(num_cells / 2) as usize][0].state = CellState::Alive;
 
+    // Go through each cell, row by row, skipping the first row,
+    // and determine whether a cell should be alive or dead given a rule
     for j in 1..num_cells as usize {
         for i in 0..num_cells as usize {
 
+            // Counter is used to index into the rule vec
             let mut counter = 0;
 
             if i != 0 && cell_grid.grid[i - 1][j - 1].state == CellState::Alive {
@@ -118,6 +133,8 @@ fn update_cell_grid_system(mut cell_grid: ResMut<CellGrid>, cell_settings: Res<C
                 counter += 1;   
             }
 
+            // If the set rule states a cell should be alive, make it alive
+            // Otherwise make it dead
             if cell_settings.rule[counter as usize] { cell_grid.grid[i][j].state = CellState::Alive}
             else { cell_grid.grid[i][j].state = CellState::Dead }
 
@@ -125,7 +142,10 @@ fn update_cell_grid_system(mut cell_grid: ResMut<CellGrid>, cell_settings: Res<C
     }
 }
 
+// System used to update sprites based on CellGrid Resource
 fn color_grid_system(mut query: Query<(&mut Sprite, &mut Cell)>, cell_grid: Res<CellGrid>, cell_settings: Res<CellSettings>) {
+    
+    // Iterate through all cells
     for (mut sprite, mut cell) in query.iter_mut() {
         cell.state = cell_grid.grid[cell.position_x as usize][cell.position_y as usize].state;
         match cell.state {
@@ -135,6 +155,7 @@ fn color_grid_system(mut query: Query<(&mut Sprite, &mut Cell)>, cell_grid: Res<
     }
 }
 
+// Used to calculate a rule array based on a given u8 value
 fn get_rule(mut rule_num: u8) -> [bool; 8] {
     let mut rule: [bool; 8] = [false; 8];
 
@@ -148,68 +169,108 @@ fn get_rule(mut rule_num: u8) -> [bool; 8] {
     return rule;
 }
 
-fn mouse_button_input_system (buttons: Res<Input<MouseButton>>, 
+// Updates rule based on mouse input
+fn mouse_button_input_system (
+    buttons: Res<Input<MouseButton>>, 
     mut cell_settings: ResMut<CellSettings>,
 ) {
+    // Right mouse button decreases rule number
     if buttons.just_pressed(MouseButton::Right) {
         cell_settings.rule_num -= 1;
         cell_settings.rule = get_rule(cell_settings.rule_num);
     }
     
+    // Left mouse button increases rule number
     if buttons.just_pressed(MouseButton::Left) {
         cell_settings.rule_num += 1;
         cell_settings.rule = get_rule(cell_settings.rule_num);
     }
 }
 
+// Update cell positions based on keyboard input
 fn key_press_system (
     keys: Res<Input<KeyCode>>,
     mut query: Query<(&mut Transform, &mut Sprite), With<Cell>>,
     mut settings: ResMut<CellSettings>,
 ) {
+    // W key moves cells up
     if keys.pressed(KeyCode::W) {
         for (mut transform, _sprite) in query.iter_mut() {
-            transform.translation.y -= 1.0 * settings.cell_size;
+            transform.translation.y -= 0.5 * settings.cell_size;
         }
     }
     
+    // S key moves cells down
     if keys.pressed(KeyCode::S) {
         for (mut transform, _sprite) in query.iter_mut() {
-            transform.translation.y += 1.0 * settings.cell_size;
+            transform.translation.y += 0.5 * settings.cell_size;
         }
     }
 
+    // D key moves cells right
     if keys.pressed(KeyCode::D) {
         for (mut transform, _sprite) in query.iter_mut() {
-            transform.translation.x -= 1.0 * settings.cell_size;
+            transform.translation.x -= 0.5 * settings.cell_size;
         }
     }
     
+    // A key moves cells left
     if keys.pressed(KeyCode::A) {
         for (mut transform, _sprite) in query.iter_mut() {
-            transform.translation.x += 1.0 * settings.cell_size;
+            transform.translation.x += 0.5 * settings.cell_size;
         }
     }
     
-    let scale_multiplier:f32 = 1.2;
+    // Constants for scaling
+    const SCALE_MULTIPLIER: f32 = 1.02;
+    const MAX_CELL_SIZE: f32 = 100.;
+    const MIN_CELL_SIZE: f32 = 1.;
     
+    // If Q is pressed, zooom in
     if keys.pressed(KeyCode::Q) {
-        settings.cell_size *= scale_multiplier;
         
-        for (mut transform, mut sprite) in query.iter_mut() {
-            sprite.custom_size = Some(Vec2::new(settings.cell_size, settings.cell_size));
-            transform.translation.x *= scale_multiplier;
-            transform.translation.y *= scale_multiplier;
+        // Change cell_size by scale multiplier
+        // If cells reach max size, do not scale anymore
+        let mut changed: bool = true;
+        settings.cell_size *= SCALE_MULTIPLIER;
+        if settings.cell_size > MAX_CELL_SIZE {
+            settings.cell_size = MAX_CELL_SIZE;
+            changed = false;
+        }
+
+        // If cells are max size, do not scale
+        if changed {
+            for (mut transform, mut sprite) in query.iter_mut() {
+                
+                // First update scale then reposition cells
+                sprite.custom_size = Some(Vec2::new(settings.cell_size, settings.cell_size));
+                transform.translation.x *= SCALE_MULTIPLIER;
+                transform.translation.y *= SCALE_MULTIPLIER;
+            }
         }
     }
     
+    // If E is pressed, zoom out
     if keys.pressed(KeyCode::E) {
-        settings.cell_size /= scale_multiplier;
+
+        // Change cell_size by scale multiplier
+        // If cells reach min size, do not scale anymore
+        let mut changed: bool = true;
+        settings.cell_size /= SCALE_MULTIPLIER;
+        if settings.cell_size < MIN_CELL_SIZE {
+            settings.cell_size = MIN_CELL_SIZE;
+            changed = false;
+        }
         
-        for (mut transform, mut sprite) in query.iter_mut() {
-            sprite.custom_size = Some(Vec2::new(settings.cell_size, settings.cell_size));
-            transform.translation.x /= scale_multiplier;
-            transform.translation.y /= scale_multiplier;
+        // If cells are min size, do not scale
+        if changed {
+            for (mut transform, mut sprite) in query.iter_mut() {
+                
+                // First update scale then reposition cells
+                sprite.custom_size = Some(Vec2::new(settings.cell_size, settings.cell_size));
+                transform.translation.x /= SCALE_MULTIPLIER;
+                transform.translation.y /= SCALE_MULTIPLIER;
+            }
         }
     }
 }
