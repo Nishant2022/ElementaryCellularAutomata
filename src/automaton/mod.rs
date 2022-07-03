d1d8e564b39c7879b9d681c9e95c72290d5ecd9a
Nishant2022 <nishant.dash@gmail.com>
2022-07-03 18:02:58 -0400

    Use system sets

--- src/automaton/mod.rs
+++ src/automaton/mod.rs
@@ -12,13 +12,28 @@ pub struct AutomataPlugin;
 impl Plugin for AutomataPlugin {
     fn build(&self, app: &mut App) {
         app
+            // Startup system
             .add_startup_system_to_stage(StartupStage::PostStartup, cell_spawn_system)
-            .add_system(update_cell_grid_system)
-            .add_system(color_grid_system.after(update_cell_grid_system))
-            .add_system(mouse_button_input_system)
-            .add_system(key_press_system)
-            .add_system(mouse_scroll_system)
-            .add_system(window_resize_system)
+
+            // Input Systems
+            .add_system_set(
+                SystemSet::new()
+                    .label("input")
+                    .with_system(mouse_button_input_system)
+                    .with_system(key_press_system)
+                    .with_system(mouse_scroll_system)
+                    .with_system(window_resize_system)
+            )
+
+            // Cell Update systems
+            .add_system_set(
+                SystemSet::new()
+                    .label("cell_update")
+                    .with_system(update_cell_grid_system.label("update_cell_grid"))
+                    .with_system(color_grid_system.after("update_cell_grid"))
+            )
+
+            // Events
             .add_event::<RuleChangeEvent>();
 
         #[cfg(target_arch = "wasm32")]
